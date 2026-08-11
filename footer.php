ecb329de743428327a43ed11a2dcbb6b22261e31
Sandeep Singh <infaterasolutions@gmail.com>
2026-08-11 23:49:52 +0530

    Update back-to-top button typography (Playfair serif italic) and use ultra-thin SVG arrow to precisely match the provided screenshot

--- footer.php
+++ footer.php
@@ -66,7 +66,9 @@
 	<!-- Back to Top Button (Stuck to right edge of footer) -->
 	<button id="back-to-top" class="figma-back-to-top" onclick="window.scrollTo({top: 0, behavior: 'smooth'});">
 		<span>back to top</span>
-		&rarr;
+		<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		  <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="#3C3C3C" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
 	</button>
 
 	<!-- Bottom Bar -->

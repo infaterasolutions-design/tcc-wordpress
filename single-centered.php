dfd1294768fdd37153a5d53937be148495e8060d
Sandeep Singh <infaterasolutions@gmail.com>
2026-08-13 03:17:36 +0530

    Update wardrobe post background color to pure white

--- single-centered.php
+++ single-centered.php
@@ -5,7 +5,7 @@
 
 get_header(); ?>
 
-<main id="main" class="site-main" style="background-color: #faf9f6; min-height: 100vh; padding-bottom: 4rem;">
+<main id="main" class="site-main" style="background-color: #ffffff; min-height: 100vh; padding-bottom: 4rem;">
 
 	<?php while ( have_posts() ) : the_post(); ?>
 

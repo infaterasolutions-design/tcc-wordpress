2275067f3ebf721399204d23ca4e554bc6727479
Sandeep Singh <infaterasolutions@gmail.com>
2026-08-13 03:13:06 +0530

    Load 'Adamina' Google Font and apply precise typography (38px size, 46px line height, weight 400) specifically to the headings of wardrobe posts

--- functions.php
+++ functions.php
@@ -77,7 +77,7 @@ add_action( 'widgets_init', 'tcc_widgets_init' );
  */
 function tcc_scripts() {
 	// Google Fonts (Optimized Payload with Elegant Scripts)
-	wp_enqueue_style( 'tcc-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&family=Playfair+Display:wght@400;700&family=Great+Vibes&family=Sacramento&family=Allura&family=Herr+Von+Muellerhoff&family=Qwigley&family=Antic+Didone&display=swap', array(), null );
+	wp_enqueue_style( 'tcc-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&family=Playfair+Display:wght@400;700&family=Great+Vibes&family=Sacramento&family=Allura&family=Herr+Von+Muellerhoff&family=Qwigley&family=Antic+Didone&family=Adamina&display=swap', array(), null );
 	
 	// Theme stylesheet
 	wp_enqueue_style( 'tcc-style', get_stylesheet_uri(), array(), TCC_VERSION );

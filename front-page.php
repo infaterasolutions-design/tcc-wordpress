a37174270fe073fd4df5ba8680d043ab38b1580f
Sandeep Singh <infaterasolutions@gmail.com>
2026-08-17 16:56:22 +0530

    Fix elsewhere text visibility: pull out of col2 to fix z-index overlap, update positioning to exact Figma coords

--- front-page.php
+++ front-page.php
@@ -401,9 +401,10 @@ get_header(); ?>
 				</picture>
 			</a>
 
+			<div class="fp-elsewhere-text">elsewhere</div>
+			
 			<!-- Column 2 -->
 			<div class="fp-elsewhere-col2">
-				<div class="fp-elsewhere-text">elsewhere</div>
 				<a href="#" class="fp-elsewhere-col2-img-wrapper">
 					<?php $img2 = "https://images.unsplash.com/photo-1617019114583-affb34d1b3cd?auto=format&fit=crop&q=80&w=400"; ?>
 					<picture class="w-full">
